a11eba190645f29e9561be3a9830f80b2ecee404
Vrishti2010 <94451805+Vrishti2010@users.noreply.github.com>
2022-12-29 17:10:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload(){
+
+
+}
+
+function setup() {
+
+canvas = createCanvas(700,500);
+canvas.center();
+
+
+}
+
+function draw(){
+}
+
+function takensnapshot(){
+
+    save("image.png");
+}
